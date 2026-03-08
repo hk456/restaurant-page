2da03e80174e572bdcd740cda9f9fa3ca2b0b49a
harish <hk04052006@gmail.com>
2026-03-08 18:22:36 +0000

    made some changes in contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,28 @@
 // contact.js 
 
 export default function loadContact() {
+    const container = Object.assign(document.createElement('div'), {
+        id: 'home',
+        className: 'container'
+    });
 
+    const heading = Object.assign(document.createElement('div'), {
+        className: 'heading',
+        textContent: 'Welcome to Our Restaurant' // Adding text so you can see it!
+    });
+
+    const content = Object.assign(document.createElement('div'), {
+        className: 'content'
+    });
+
+    const h = Object.assign(document.createElement('div'), {
+        className: 'title'
+    });
+    const p = Object.assign(document.createElement('div'), {
+        className: 'para'
+    });
+
+    content.append(h,p);
+    container.append(heading, content, content, content);
+    
 }
