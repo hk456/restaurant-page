200142d93ac3db312d06d20f52dad0e8c7ff9c0e
harish <hk04052006@gmail.com>
2026-03-08 19:24:56 +0000

    made some questionable style changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import loadContact from './contact.js';
 const mainContent = document.getElementById('content');
 const homeTab = document.querySelector('.home');
 const menuTab = document.querySelector('.menu');
-const aboutTab = document.querySelector('.about');
+const contactTab = document.querySelector('.contact');
+
+mainContent.replaceChildren(loadHome());
 
 homeTab.addEventListener('click', () => {
     mainContent.replaceChildren(loadHome());
@@ -17,6 +19,6 @@ menuTab.addEventListener('click', () => {
     mainContent.replaceChildren(loadMenu());
 });
 
-aboutTab.addEventListener('click', () => {
+contactTab.addEventListener('click', () => {
     mainContent.replaceChildren(loadContact());
 });
