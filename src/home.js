e92e76b38fb518bd5f1e1c7bf744cb94ad28a12d
harish <hk04052006@gmail.com>
2026-03-09 12:45:00 +0000

    just... not getting inspiration will come back to all this stuff later

--- src/home.js
+++ src/home.js
@@ -11,26 +11,47 @@
 */
 
 export default function loadHome(){
-    const container = Object.assign(document.createElement('div'), {
-        id: 'home',
-        className: 'container'
-    });
+    const container = Object.assign(document.createElement('div'), { id: 'home', className: 'container' });
+    const p = Object.assign(document.createElement('p'));
+    const h = Object.assign(document.createElement('h3'));
 
-    const heading = Object.assign(document.createElement('div'), {
-        className: 'heading',
-        textContent: 'Welcome to Our Restaurant' // Adding text so you can see it!
-    });
+    const heading = Object.assign(document.createElement('div'), { className: 'heading', textContent: 'Welcome to Our Restaurant' });
 
-    const content = Object.assign(document.createElement('div'), {
-        className: 'content'
-    });
+    const content = Object.assign(document.createElement('div'), { className: 'content' });
 
     container.append(heading, content);
 
     // Creating the sub-sections
-    const about = Object.assign(document.createElement('div'), { className: 'about', textContent: 'About Us' });
-    const hours = Object.assign(document.createElement('div'), { className: 'hours', textContent: 'Opening Hours' });
-    const contact = Object.assign(document.createElement('div'), { className: 'contact', textContent: 'Contact Info' });
+    // About 
+    const about = Object.assign(document.createElement('div'), { className: 'about' });
+    h.textContent = ''
+    about.append(p, h);
+
+    // Hours
+    const hours = Object.assign(document.createElement('div'), { className: 'hours' });
+    const ul = Object.assign(document.createElement('div'));
+   
+    const timetable = [
+        { day: "Sunday",    start: "8am", end: "8pm" },
+        { day: "Monday",    start: "6am", end: "6pm" },
+        { day: "Tuesday",   start: "6am", end: "6pm" },
+        { day: "Wednesday", start: "6am", end: "6pm" },
+        { day: "Thursday",  start: "6am", end: "10pm" },
+        { day: "Friday",    start: "6am", end: "10pm" },
+        { day: "Saturday",  start: "8am", end: "10pm" }
+    ];
+
+    timetable.forEach(item => {
+        const li = document.createElement('div');
+        li.textContent = `${item.day}: ${item.start} - ${item.end}`;
+        ul.append(li);
+    });
+
+    hours.appendChild(ul);
+
+    // Contact
+    const contact = Object.assign(document.createElement('div'), { className: 'contact' });
+    contact.append(h, p);
 
     content.append(about, hours, contact);   
     
